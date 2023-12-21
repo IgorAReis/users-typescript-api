85597292aa348cf4d97f100b3c62043a88cc0758
Igor Aleixo Reis <igor_a.reis@hotmail.com>
2023-12-21 18:32:18 -0300

    feat: add CreateUserController with validation

--- src/controllers/create-user/protocols.ts
+++ src/controllers/create-user/protocols.ts
@@ -1,10 +1,15 @@
 import { User } from "../../models/user";
+import { HttpRequest, HttpResponse } from "../protocols";
+
+export interface ICreateUserController {    
+    handle(httpRequest: HttpRequest<CreateUserParams>): Promise<HttpResponse<User>>;
+}
 
 export interface CreateUserParams {
     firstName: string;
     lastName: string;
-    password: string;
     email: string;
+    password: string;
 }
 
 export interface ICreateUserRepository {
--- src/controllers/protocols.ts
+++ src/controllers/protocols.ts
@@ -1,4 +1,11 @@
-export interface HttpResponse <T> {
-    statusCode: number
-    body: T | string;
+/* eslint-disable @typescript-eslint/no-explicit-any */
+export interface HttpResponse<T> {
+  statusCode: number;
+  body: T | string;
+}
+
+export interface HttpRequest<B>{
+  params?: any;
+  headers?: any;
+  body?: B;
 }
